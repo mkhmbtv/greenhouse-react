288f1eaeb68d2448d982bbf0cf235d40468b5bfb
your name here <mmakhambetov@yandex.ru>
2021-12-01 22:36:11 +0400

    Complete phases 5 - 6

--- src/components/Hygrometer/Hygrometer.js
+++ src/components/Hygrometer/Hygrometer.js
@@ -1,14 +1,17 @@
 import ReactSlider from "react-slider";
+import { useClimate } from "../../context/ClimateContext";
 import "./Hygrometer.css";
 
 function Hygrometer() {
+  const { humidity, setHumidity } = useClimate();
+
   return (
     <section>
       <h2>Hygrometer</h2>
-      <div className="actual-humid">Actual Humidity: {"y"}%</div>
+      <div className="actual-humid">Actual Humidity: {humidity}%</div>
       <ReactSlider
-        value={10}
-        onAfterChange={(val) => {}}
+        value={humidity}
+        onAfterChange={(val) => {setHumidity(val)}}
         className="hygrometer-slider"
         thumbClassName="hygrometer-thumb"
         trackClassName="hygrometer-track"
